24f3b6e27d5ed71bf123ec151d15243f2ff608e4
Sornnacha Buranapongwattana <sornnacha.bu@gmail.com>
2025-04-18 01:09:55 +0700

    update

--- src/components/Copyright.js
+++
@@ -1,9 +0,0 @@
-const CopyRight = () =>{
-    return (
-    <div className="bg-white"  style={{height:'50px'}}>
-        <p className="text-dark pt-3" style={{textAlign:'center'}}>Max Studio 2023 &copy; LOGO|Blog from MERN-stack developing</p>
-    </div>
-    )
-}
-
-export default CopyRight
